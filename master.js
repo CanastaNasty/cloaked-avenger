798f348b3793f97d08153e8dc0c77257aa10acb7
CanastaNasty <canasta.nasty@gmail.com>
2013-10-04 19:18:54 -0700

    :deciduous_tree::tent: Updated in browser at strd6.github.io/editor

--- master.js
+++ master.js
@@ -1,5 +1,15 @@
 (function() {
-  alert("Hey!");
+  var Game, game, template;
+
+  Game = require("./game");
+
+  game = Game({
+    points: 500
+  });
+
+  template = require("./templates/main");
+
+  $("body").append(template(game));
 
 }).call(this);
 
